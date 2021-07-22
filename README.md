d7e7f1a2ea77f4ea84f20df377a3d458ccf82fb3
Andy <andrewprohde@gmail.com>
2021-07-21 21:17:52 -0500

    added rough framework

---
+++ README.md
@@ -0,0 +1,3 @@
+Comic Tracker
+
+The purpose of this application is to document comic issues that I have read. 
--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 //If comic has been read, read the API info for that issue and store it in Mongo. Build display on site. Overlay a green check-mark on the corner to signify read
 
+var api_key="e4de14d13e7c5b99c4917f00d57d51b6";
+var api_hash="a0a34538942beb2ec01d018474aa32f58265850d"
+var api_base="https://gateway.marvel.com:443/"
+var timestamp=Date.now()
